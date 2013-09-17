682edb7b8e81621bc885d226f0d15a7da6765d04
cayden <cuiran2001@163.com>
2013-09-17 15:43:32 +0800

    cc

--- src/Test2.java
+++ src/Test2.java
@@ -17,7 +17,7 @@ public class Test2 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello GitHub!");
+		System.out.println("Hello GitHub!!!");
 		
 		
 		
