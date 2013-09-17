ed38c437ba3ba356f86e7a44ce56569d7b9ce552
cayden <cuiran2001@163.com>
2013-09-17 15:31:42 +0800

    first commit

---
+++ src/Test.java
@@ -0,0 +1,23 @@
+/**
+ * Test.java
+ * 版权所有(C) 2013 
+ * 创建:cuiran 2013-9-17 下午3:25:22
+ */
+
+/**
+ * TODO
+ * @author cuiran
+ * @version 1.0.0
+ */
+public class Test {
+
+	/**
+	 * TODO
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello GitHub!");
+	}
+
+}
