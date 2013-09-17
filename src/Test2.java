6c1b67e4384b34989a2d59bd023d5d945376a52f
cayden <cuiran2001@163.com>
2013-09-17 15:37:06 +0800

    第二次

---
+++ src/Test2.java
@@ -0,0 +1,23 @@
+/**
+ * Test2.java
+ * 版权所有(C) 2013 
+ * 创建:cuiran 2013-9-17 下午3:36:26
+ */
+
+/**
+ * TODO
+ * @author cuiran
+ * @version 1.0.0
+ */
+public class Test2 {
+
+	/**
+	 * TODO
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello GitHub!");
+	}
+
+}
